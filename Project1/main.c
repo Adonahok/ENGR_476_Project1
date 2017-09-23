945af3c2d31e04156642f047104268c8d73561f8
Adonahok <ccestes94@gmail.com>
2017-09-23 10:05:14 -0700

    Still reading file, put down very basic code to do so.

--- Project1/main.c
+++ Project1/main.c
@@ -19,8 +19,64 @@
  */
 int main(int argc, char** argv) {
     
+    char students[7][7];
+    int i = 0, j = 0;
+    int ID[7];
+    int k = 0;
+    float SubA[7];
+    int l = 0;
+    float SubB[7];
+    int m = 0;
+    
+    int p = 0;
+    
+    int c;
+    
     // open file 
     // 'Final Scores' 
+    int num;
+   FILE *frptr;
+   frptr = fopen("C:\\Final Scores.txt","r");
+
+   if(frptr == NULL)
+   {
+      printf("Error, No 'Final Scores' File Detected");   
+      exit(1);             
+   }
+   
+   while(frptr != EOF) {
+       c = getc(frptr);
+       
+       if(c == '\t') {
+           c = getc(frptr);
+       }
+       else {
+           switch(p){
+               case 0:
+                   students[i][j] = c;
+                   j++;
+                   break;
+               case 1:
+                   ID[k] = c;
+                   k++;
+                   break;
+               case 2:
+                   SubA[l] = c;
+                   l++;
+                   break;
+               case 3:
+                   SubB[m] = c;
+                   m++;
+                   break;
+               default:
+                   break;
+           }
+                           
+           }
+       }
+   }
+
+   
     
     //read data from file
     // either use a tokenizer or parser
